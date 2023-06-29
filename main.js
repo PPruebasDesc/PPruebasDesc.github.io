daabfa1a2de428edcca2b9c4be4337c81f7b8329
PPruebasDesc <137747611+PPruebasDesc@users.noreply.github.com>
2023-06-29 00:26:38 +0000

    aña

--- main.js
+++ main.js
@@ -20,22 +20,3 @@ const firebaseConfig = {
   appId: "1:161895581783:web:aee7fe1d1b90a1ae047b12",
   measurementId: "G-P4H2FNEPQ1"
 };
-
-
-// Inicializa Firebase
-const app = initializeApp(firebaseConfig);
-
-// Inicializa la autenticación de Firebase y obtén una referencia al servicio
-const auth = getAuth(app);
-
-
-// Firebase imports
-// Importa las funciones necesarias de los SDK que necesites
-import { signInWithPopup } from "firebase/auth";
-import { initializeApp} from "firebase/app";
-import { getAuth, signInWithPopup } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-
-
-const analytics = getAnalytics(app);
