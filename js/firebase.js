e8cf15c5fe191428bc03af806bc35e6068429449
PPruebasDesc <137747611+PPruebasDesc@users.noreply.github.com>
2023-06-29 01:04:37 +0000

    aña

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  // Paste your firebase config here
+  apiKey: "AIzaSyDTdKRahhF220DwFH4RVWmpFxs0WjJ8rbA",
+  authDomain: "pruebasconfig-c2268.firebaseapp.com",
+  databaseURL: "https://pruebasconfig-c2268-default-rtdb.firebaseio.com",
+  projectId: "pruebasconfig-c2268",
+  storageBucket: "pruebasconfig-c2268.appspot.com",
+  messagingSenderId: "161895581783",
+  appId: "1:161895581783:web:aee7fe1d1b90a1ae047b12",
+  measurementId: "G-P4H2FNEPQ1"
+};
--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+import { auth, db } from "./app/firebase.js";
+import { loginCheck } from "./app/loginCheck.js";
+import { setupPosts } from "./app/postList.js";
+
+import './js/CtrlBotones.js'
+import './js/firebase.js'
+import './js/login.js'
+import './js/logout.js'
+import './js/postList.js'
+import './js/register.js'
+import './js/showMessage.js'
+
+// list for auth state changes
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    loginCheck(user);
+    try {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+      setupPosts(querySnapshot.docs);
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
+
+
+
+
 // JS Imports
 import { showRegister, showLogin, login, register, google, GoogleAuthProvider } from "./js/CtrlBotones";
 
@@ -5,18 +38,16 @@ import { showRegister, showLogin, login, register, google, GoogleAuthProvider } 
 import{sendMail,nodemailer,}  from "nodemailer";
 
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
 
+
+
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
 // TODO: Reemplaza la siguiente configuración con la configuración de tu proyecto Firebase
-
-  // Tu configuración de Firebase aquí
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDTdKRahhF220DwFH4RVWmpFxs0WjJ8rbA",
-  authDomain: "pruebasconfig-c2268.firebaseapp.com",
-  databaseURL: "https://pruebasconfig-c2268-default-rtdb.firebaseio.com",
-  projectId: "pruebasconfig-c2268",
-  storageBucket: "pruebasconfig-c2268.appspot.com",
-  messagingSenderId: "161895581783",
-  appId: "1:161895581783:web:aee7fe1d1b90a1ae047b12",
-  measurementId: "G-P4H2FNEPQ1"
-};
